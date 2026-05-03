011276cd97a2b757c3008f6e440db985aa723078
BertrandOscarSaputra <bertrandosc@gmail.com>
2026-05-04 07:44:25 +0800

    fix:port

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:2323/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3535/api";
 
 const api = axios.create({
   baseURL: API_URL,
